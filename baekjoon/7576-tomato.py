2d2883fe038ab2381865ba31d2b0ef0379e9b410
legulation <rbwo1022@gmail.com>
2026-02-10 22:46:09 +0900

    tomato solve

--- baekjoon/7576-tomato.py
+++ baekjoon/7576-tomato.py
@@ -6,30 +6,39 @@ input = sys.stdin.readline
 M, N = map(int, input().split())
 
 graph = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
 
-for r in N:
-    for c in M:
+queue = deque([])
+
+for r in range(N):
+    for c in range(M):
         if graph[r][c] == 1:
             start_r = r
             start_c = c
+            queue.append((start_r, start_c))
+            visited[r][c] = 1
 
-queue = deque([(start_r, start_c)])
+dr = [0, 0, -1, 1]
+dc = [-1, 1, 0, 0]
 
 while queue:
-    n = queue.popleft()
+    x, y = queue.popleft()
 
-    
-'''
-visited가 필요한가?
-> graph가 있어서 그냥 0을 1로 만들면서 가면 될 것 같음
+    for i in range(4):
+        nx = x + dr[i]
+        ny = y + dc[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[nx][ny] == 0 and not visited[nx][ny]:
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx, ny))
 
-그럼 depth는 어떻게 확인?
-> 이동할 때마다 depth +1?
+ans = 0
+for r in range(N):
+    for c in range(M):
+        if graph[r][c] == 0 and visited[r][c] == 0:
+            print(-1)
+            exit()
+        
+        ans = max(ans, visited[r][c])
 
-dir 사용?
-
-행 순회를 하는데, queue를 어떻게 써야할까?
-> 그래서 원래는 노드 번호를 큐에 집어넣고 빼고 했는데 지금은 2차원 리스트(행렬)에서 앞옆뒤를 갈 수 있냐마냐를 확인해야됨
-> 2차원 리스트의 전체를 다 간선으로 연결할 수는 없음
-> 그래서 튜플 형태로 (r, c)를 집어넣어야 하나 싶음
-'''
+print(ans - 1)
