ae2f5700d9d57d7a766663df0e272c4bdc158a0d
legulation <rbwo1022@gmail.com>
2026-03-11 22:28:16 +0900

    10820 solve

---
+++ baekjoon/10820-string.py
@@ -0,0 +1,17 @@
+import sys
+
+for line in sys.stdin:
+    line = line.rstrip('\n')
+
+    if not line and not line.isspace():
+        pass
+
+    lower, upper, digit, space = 0, 0, 0, 0
+
+    for char in line:
+        if char.islower(): lower += 1
+        elif char.isupper(): upper += 1
+        elif char.isdigit(): digit += 1
+        elif char.isspace(): space += 1
+
+    print(f"{lower} {upper} {digit} {space}")
